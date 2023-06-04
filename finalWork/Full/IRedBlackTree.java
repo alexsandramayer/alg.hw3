2f3b090bdba8ebd9910886db59e898c7e5cdf555
Alexsandra Mayer <97391174+alexsandramayer@users.noreply.github.com>
2023-06-04 13:01:32 +0500

    Add files via upload

---
+++ finalWork/Full/IRedBlackTree.java
@@ -0,0 +1,26 @@
+package algorithms.finalWork.Full;
+
+
+
+
+public interface IRedBlackTree<T extends Comparable<T>> 
+{
+	
+	/**
+    * Добавить элемент в дерево
+    * @param o
+    */
+    void add(T o);
+    
+    /**
+     * Удалить элемент из дерева
+     * @param o
+     */
+    boolean remove(T o);
+    
+    /**
+     * Возвращает true, если элемент содержится в дереве
+     * @param o
+     */
+    boolean contains(T o);
+}
---
+++ finalWork/finalProject.java
@@ -0,0 +1,16 @@
+package algorithms.finalWork;
+
+public class finalProject 
+{
+    
+    public static void main(String[] args) 
+    {
+        final  RedBlackTree tree = new RedBlackTree();
+        for (int i = 0; i < 15; i++) 
+        {
+            tree.add(i);
+        }
+        tree.print();
+
+    }
+}
